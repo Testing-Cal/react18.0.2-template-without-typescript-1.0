863f84859bfb23e0fcd78801e67522a366b0f1c9
Abhijeet Kamble <akamble@calibo.com>
2024-03-22 12:17:47 +0530

    fix for nested routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import * as React from 'react'
+import { createRoot } from 'react-dom/client';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Home from './home';
-import Dashboard from './dashboard-page';
-import Listing from './listing-page';
-import PageNotFound from './page-not-found';
+import { Outlet } from "react-router-dom";
+import './style.css';
+import Dashboard  from "./dashboard-page";
+import Listing from "./listing-page";
+import Nested from "./nested";
+import PageNotFound from './page-not-found'
+console.log("Inside index.tsx")
+
 
 const App = () => {
   const basePath = process.env.REACT_APP_CONTEXT;
@@ -12,15 +18,18 @@ const App = () => {
     <Router basename={basePath}>
       <div>
           <nav style={{margin: '20px'}}>
-              <Link to="/" style={{marginRight: '20px'}}>Home1</Link>
+              <Link to="/" style={{marginRight: '20px'}}>Home</Link>
               <Link to="/dashboard" style={{marginRight: '20px'}}>Dashboard</Link>
-              <Link to="/listing">Listing</Link>
-              <Link to="/dashboard/listing">Listing 1</Link>
+              <Link to="/listing" style={{marginRight: '20px'}}>Listing</Link>
+              <Link to="/dashboard/listing">About</Link>
+
           </nav>
+
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/dashboard/*" element={<Dashboard />} >
                     <Route index element={<Home />} />
+                    <Route path='nested' element={<Nested />} /> {/*A nested route!*/}
                     <Route path='listing' element={<Listing />} /> {/*A nested route!*/}
               </Route>
               <Route path="/listing" element={<Listing />} />
@@ -31,40 +40,61 @@ const App = () => {
    )
 }
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error("Root element not found.");
+}
+
 
 /*
 class based component
 
-class App extends Component {
-  constructor() {
-    super();
+type AppState = {
+    name: string
+}
+
+type AppProp = {
+
+}
+
+class App extends React.Component<AppProp,AppState> {
+  constructor(props:AppProp) {
+    super(props);
     this.state = {
       name: 'Welcome to Integrated Digital Platform'
     };
   }
 
   render() {
-   const basePath = process.env.REACT_APP_CONTEXT;
-    return (
-        <Router basename={basePath}>
-            <div>
-                <nav style={{margin: '20px'}}>
-                    <Link to="/" style={{marginRight: '20px'}}>Home</Link>
-                    <Link to="/dashboard" style={{marginRight: '20px'}}>Dashboard</Link>
-                    <Link to="/listing">Listing</Link>
-                </nav>
-                <Routes>
-                    <Route path="/" element={<Home name={this.state.name}/>} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/listing" element={<Listing />} />
-                    <Route path="*" element={<PageNotFound />} />
-                </Routes>
-            </div>
-        </Router>
-    )
+    const basePath = process.env.REACT_APP_CONTEXT;
+      return (
+          <Router basename={basePath}>
+              <div>
+                  <nav style={{margin: '20px'}}>
+                      <Link to="/" style={{marginRight: '20px'}}>Home</Link>
+                      <Link to="/dashboard" style={{marginRight: '20px'}}>Dashboard</Link>
+                      <Link to="/listing">Listing</Link>
+                  </nav>
+                  <Routes>
+                      <Route path="/" element={<Home name={this.state.name}/>} />
+                      <Route path="/dashboard" element={<Dashboard />} />
+                      <Route path="/listing" element={<Listing />} />
+                      <Route path="*" element={<PageNotFound />} />
+                  </Routes>
+              </div>
+          </Router>
+      )
   }
 }
 
-render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error("Root element not found.");
+}
 */
